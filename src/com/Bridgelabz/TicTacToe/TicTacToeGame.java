c68f377e45e2d1b0601321287923a016bdd7f0de
Ankitkumar147 <aankitchouhan3@gmail.com>
2022-08-07 16:59:27 +0530

    Added Show Board method for selecting the valid empty cell

--- src/com/Bridgelabz/TicTacToe/TicTacToeGame.java
+++ src/com/Bridgelabz/TicTacToe/TicTacToeGame.java
@@ -1,6 +1,5 @@
 package com.Bridgelabz.TicTacToe;
 
-
 import java.util.Scanner;
 
 public class TicTacToeGame {
@@ -9,17 +8,18 @@ public class TicTacToeGame {
     public static Scanner sc1 = new Scanner(System.in);
     static char computerChoice;
 
-
     public static void main(String[] args) {
         createBoard();
         showBoard();
         characterChoice();
     }
+
     public static void createBoard(){
         for (int i = 1; i < board.length; i++) {
             board[i] = ' ';
         }
     }
+
     public static void showBoard(){
         System.out.println("Displaying board for the Game.\n");
         System.out.println(board[1]+"  | "+board[2]+"| "+board[3]);
@@ -28,6 +28,7 @@ public class TicTacToeGame {
         System.out.println("---+--+---");
         System.out.println(board[7]+"  | "+board[8]+"| "+board[9]);
     }
+
     public static void characterChoice() {
         System.out.println("\nSelect Any Character X or O ");
         char playerChoice = sc1.next().toUpperCase().charAt(0);
