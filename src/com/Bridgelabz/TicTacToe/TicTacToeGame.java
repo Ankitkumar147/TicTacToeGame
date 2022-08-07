0655d067dda334e686383f49d399df47a9eda93c
Ankitkumar147 <aankitchouhan3@gmail.com>
2022-08-07 16:55:32 +0530

    Added : Added Choose letter method to read user input and start the game.

--- src/com/Bridgelabz/TicTacToe/TicTacToeGame.java
+++ src/com/Bridgelabz/TicTacToe/TicTacToeGame.java
@@ -1,12 +1,19 @@
 package com.Bridgelabz.TicTacToe;
 
+
+import java.util.Scanner;
+
 public class TicTacToeGame {
 
     public static char[] board = new char[10];
+    public static Scanner sc1 = new Scanner(System.in);
+    static char computerChoice;
+
 
     public static void main(String[] args) {
         createBoard();
         showBoard();
+        characterChoice();
     }
     public static void createBoard(){
         for (int i = 1; i < board.length; i++) {
@@ -21,7 +28,17 @@ public class TicTacToeGame {
         System.out.println("---+--+---");
         System.out.println(board[7]+"  | "+board[8]+"| "+board[9]);
     }
-
+    public static void characterChoice() {
+        System.out.println("\nSelect Any Character X or O ");
+        char playerChoice = sc1.next().toUpperCase().charAt(0);
+        if (playerChoice == 'X') {
+            computerChoice = 'O';
+            System.out.println("Player chose X so the computer choice is O");
+        } else if ( playerChoice == 'O') {
+            computerChoice = 'X';
+            System.out.println("Player chose O so the computer choice is X");
+        }
+    }
 }
 
 
